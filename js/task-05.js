f779deb37277ef956a10231ffc288f324da2891a
Ruslan <shamiltat79@gmail.com>
2022-10-26 00:23:13 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,15 @@
 const input = document.querySelector('#name-input');
 // console.log(input);
 const inputName = document.querySelector('#name-output');
-// console.log(inputName);
+console.log(inputName);
+
+
 const inputTextChange = (event) => {
     console.log(event.currentTarget.value);
     inputName.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        inputName.textContent = "Anonymous";
+    }
 };
+
 input.addEventListener('input', inputTextChange);
